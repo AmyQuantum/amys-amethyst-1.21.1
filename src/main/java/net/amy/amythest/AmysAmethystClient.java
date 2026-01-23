b4ac57326caad458ce5d19c9f94bc1785ce5c75b
Thunderface44 <ianto.bevan@icloud.com>
2026-01-23 19:32:29 +0000

    initial commit

---
+++ src/main/java/net/amy/amythest/AmysAmethystClient.java
@@ -0,0 +1,10 @@
+package net.amy.amythest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AmysAmethystClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
