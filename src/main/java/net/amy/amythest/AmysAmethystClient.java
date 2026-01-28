c61831c59f39f0b95af47899570451c5f5dc3e81
Amythest <ianto.bevan@icloud.com>
2026-01-28 00:39:13 +0000

    Amethyst Trident Pt. 1

--- src/main/java/net/amy/amythest/AmysAmethystClient.java
+++ src/main/java/net/amy/amythest/AmysAmethystClient.java
@@ -1,19 +1,24 @@
 package net.amy.amythest;
 
+
 import net.amy.amythest.client.render.entity.AmethystArrowEntityRenderer;
 import net.amy.amythest.client.render.entity.AmethystTridentEntityRenderer;
 import net.amy.amythest.entity.ModEntityType;
 import net.amy.amythest.entity.projectile.AmethystArrowEntity;
 import net.amy.amythest.item.ModItems;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
 import net.minecraft.client.render.entity.ArrowEntityRenderer;
+import net.minecraft.resource.ResourceType;
+import net.minecraft.client.MinecraftClient;
 
-public class AmysAmethystClient implements ClientModInitializer{
+
+public class AmysAmethystClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(ModEntityType.AMETHYST_ARROW, context -> new AmethystArrowEntityRenderer(context));
-        EntityRendererRegistry.register(ModEntityType.AMETHYST_TRIDENT, context -> new AmethystTridentEntityRenderer(context));
+        EntityRendererRegistry.register(ModEntityType.AMETHYST_ARROW, AmethystArrowEntityRenderer::new);
+        EntityRendererRegistry.register(ModEntityType.AMETHYST_TRIDENT, AmethystTridentEntityRenderer::new);
     }
 }
-
