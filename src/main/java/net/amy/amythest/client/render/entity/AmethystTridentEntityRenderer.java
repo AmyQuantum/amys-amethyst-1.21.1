7c4852ac8f8286ec9594a5f23e07dc1953d65a62
Amythest <ianto.bevan@icloud.com>
2026-01-28 00:39:22 +0000

    Amethyst Trident Pt. 1

---
+++ src/main/java/net/amy/amythest/client/render/entity/AmethystTridentEntityRenderer.java
@@ -0,0 +1,46 @@
+package net.amy.amythest.client.render.entity;
+
+import net.amy.amythest.AmysAmethyst;
+import net.amy.amythest.client.render.entity.model.AmethystTridentEntityModel;
+import net.amy.amythest.entity.projectile.AmethystTridentEntity;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.EntityRenderer;
+import net.minecraft.client.render.entity.EntityRendererFactory;
+import net.minecraft.client.render.entity.model.EntityModelLayers;
+import net.minecraft.client.render.entity.model.TridentEntityModel;
+import net.minecraft.client.render.item.ItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RotationAxis;
+
+@Environment(EnvType.CLIENT)
+public class AmethystTridentEntityRenderer extends EntityRenderer<AmethystTridentEntity> {
+	public static final Identifier TEXTURE = Identifier.of(AmysAmethyst.MOD_ID, "textures/entity/amethyst_trident.png");
+	private final AmethystTridentEntityModel model;
+
+	public AmethystTridentEntityRenderer(EntityRendererFactory.Context context) {
+		super(context);
+		this.model = new AmethystTridentEntityModel(context.getPart(EntityModelLayers.TRIDENT));
+	}
+
+	public void render(AmethystTridentEntity amethystTridentEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+		matrixStack.push();
+		matrixStack.multiply(RotationAxis.POSITIVE_Y.rotationDegrees(MathHelper.lerp(g, amethystTridentEntity.prevYaw, amethystTridentEntity.getYaw()) - 90.0F));
+		matrixStack.multiply(RotationAxis.POSITIVE_Z.rotationDegrees(MathHelper.lerp(g, amethystTridentEntity.prevPitch, amethystTridentEntity.getPitch()) + 90.0F));
+		VertexConsumer vertexConsumer = ItemRenderer.getDirectItemGlintConsumer(
+			vertexConsumerProvider, this.model.getLayer(this.getTexture(amethystTridentEntity)), false, amethystTridentEntity.isEnchanted()
+		);
+		this.model.render(matrixStack, vertexConsumer, i, OverlayTexture.DEFAULT_UV);
+		matrixStack.pop();
+		super.render(amethystTridentEntity, f, g, matrixStack, vertexConsumerProvider, i);
+	}
+
+	public Identifier getTexture(AmethystTridentEntity amethystTridentEntity) {
+		return TEXTURE;
+	}
+}
---
+++ src/main/java/net/amy/amythest/client/render/entity/model/AmethystTridentEntityModel.java
@@ -0,0 +1,41 @@
+package net.amy.amythest.client.render.entity.model;
+
+import net.amy.amythest.AmysAmethyst;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.model.*;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.entity.model.TridentEntityModel;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
+
+
+@Environment(EnvType.CLIENT)
+public class AmethystTridentEntityModel extends Model {
+	public static final Identifier TEXTURE = Identifier.of(AmysAmethyst.MOD_ID, "textures/entity/amethyst_trident.png");
+	private final ModelPart root;
+
+	public AmethystTridentEntityModel(ModelPart root) {
+		super(RenderLayer::getEntitySolid);
+		this.root = root;
+	}
+
+	public static TexturedModelData getTexturedModelData() {
+		ModelData modelData = new ModelData();
+		ModelPartData modelPartData = modelData.getRoot();
+		ModelPartData modelPartData2 = modelPartData.addChild(
+			"pole", ModelPartBuilder.create().uv(0, 6).cuboid(-0.5F, 2.0F, -0.5F, 1.0F, 25.0F, 1.0F), ModelTransform.NONE
+		);
+		modelPartData2.addChild("base", ModelPartBuilder.create().uv(4, 0).cuboid(-1.5F, 0.0F, -0.5F, 3.0F, 2.0F, 1.0F), ModelTransform.NONE);
+		modelPartData2.addChild("left_spike", ModelPartBuilder.create().uv(4, 3).cuboid(-2.5F, -3.0F, -0.5F, 1.0F, 4.0F, 1.0F), ModelTransform.NONE);
+		modelPartData2.addChild("middle_spike", ModelPartBuilder.create().uv(0, 0).cuboid(-0.5F, -4.0F, -0.5F, 1.0F, 4.0F, 1.0F), ModelTransform.NONE);
+		modelPartData2.addChild("right_spike", ModelPartBuilder.create().uv(4, 3).mirrored().cuboid(1.5F, -3.0F, -0.5F, 1.0F, 4.0F, 1.0F), ModelTransform.NONE);
+		return TexturedModelData.of(modelData, 32, 32);
+	}
+
+	@Override
+	public void render(MatrixStack matrices, VertexConsumer vertices, int light, int overlay, int color) {
+		this.root.render(matrices, vertices, light, overlay, color);
+	}
+}
